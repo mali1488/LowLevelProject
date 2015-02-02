4bfd2cb79caedb9e6d5da2cadc2ed7715b2dba58
Mattias <mattias.linder87@gmail.com>
2015-02-02 16:10:36 +0100

    fixed script file

--- lab1/demo/stats.sh
+++ lab1/demo/stats.sh
@@ -1,55 +1,55 @@
 echo "seq 100 run\n" >> data.txt
 
-for V in {1..3}
+for V in {1..100}
  do 
     ./demo --seq --timing-mode
 done
 
-echo "pthread 2 100 run \n" << data.txt
+echo "pthread 2 100 run \n" >> data.txt
 
 for E in {1..100}
  do 
     ./demo --pthread threads=2 --timing-mode
 done
 
-echo "pthread 4 100 run \n" << data.txt
+echo "pthread 4 100 run \n" >> data.txt
 
 for V in {1..100}
  do 
     ./demo --pthread threads=4 --timing-mode
 done
 
-echo "pthread 8 100 run \n" << data.txt
+echo "pthread 8 100 run \n" >> data.txt
 for V in {1..100}
  do 
     ./demo --pthread threads=8 --timing-mode
 done
 
-echo "pthread 16 100 run \n" << data.txt
+echo "pthread 16 100 run \n" >> data.txt
 for V in {1..100}
  do 
     ./demo --pthread threads=16 --timing-mode
 done
 
-echo "omp 2 100 run \n" << data.txt
+echo "omp 2 100 run \n" >> data.txt
 for V in {1..100}
  do 
     ./demo --omp --threads=2 --timing-mode
 done
 
-echo "omp 4 100 run \n" << data.txt
+echo "omp 4 100 run \n" >> data.txt
 for V in {1..100}
  do 
     ./demo --omp --threads=4 --timing-mode
 done
 
-echo "omp 8 100 run \n" << data.txt
+echo "omp 8 100 run \n" >> data.txt
 for V in {1..100}
  do 
     ./demo --omp --threads=8 --timing-mode
 done
 
-echo "omp 16 100 run \n" << data.txt
+echo "omp 16 100 run \n" >> data.txt
 for V in {1..100}
  do 
     ./demo --omp --threads=16 --timing-mode
